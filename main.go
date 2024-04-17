8135e0f566b3d2740eeb070f703b09f9a33d884e
KingDaemonX <adetoyeanointing15@gmail.com>
2024-04-17 15:59:09 +0100

    sum all even number in a slice

--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	log.Println(section1.SwapVariableByReassigning(1, 2))
 	log.Println(section1.SwapVariableWithReturn(9, 7))
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(section1.SumEvenNumberInSlice(nums))
 }
--- section_1/slice.go
+++ section_1/slice.go
@@ -1,1 +1,13 @@
 package section1
+
+func SumEvenNumberInSlice(nums []int) int {
+	result := 0
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			result += num
+		}
+	}
+
+	return result
+}
