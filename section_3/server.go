b3370fbbe046f78746732712b3ea0190e6ab6b09
KingDaemonX <adetoyeanointing15@gmail.com>
2024-04-17 17:25:43 +0100

    simple http server

--- section_3/server.go
+++ section_3/server.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server listening on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
